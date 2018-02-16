8a1f4100feeae48ba9a9f312a9446d6888af13ab
snowmaniowa <santharaam@gmail.com>
2018-02-15 18:09:30 -0600

    initial commit

---
+++ das-boot/src/main/java/com/boot/App.java
@@ -0,0 +1,19 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(App.class, args);
+	}
+	
+// what are the steps after this?	
+//	1. Controller package
+//	2. Model package
+//	3. Repository package
+//	4. Service package
+}
